5a7a3b34e9566d7639fe3fbc807bde51bbfb50df
VGODIE <mle-user@epdqdqct3j5qa2lin3u2.auto.internal>
2024-04-14 21:18:26 +0000

    fixes

--- app/main.py
+++ app/main.py
@@ -1,19 +1,35 @@
-# импорт библиотеки для работы со случайными числами
+# импортируем библиотеку для работы со случайными числами
 import random
 
-# импорт класса для создания экземпляра FastAPI приложения
+# импортируем класс для создания экземпляра FastAPI приложения
 from fastapi import FastAPI
 
-# создание экземпляра FastAPI приложения
+# создаём экземпляр FastAPI приложения
 app = FastAPI()
 
-# обработка запросов к корню приложения
+# обрабатываем запросы к корню приложения
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# обработка запросов к специальному пути для получения предсказания модели
+# обработка GET-запросов к URL /service-status
+@app.get("/service-status")
+def health_check():
+    return {"status": "ok"}
+
+# обрабатываем запросы к специальному пути для получения предсказания модели
 # предсказание пока что в виде заглушки со случайной генерацией score
-@app.get("/api/public/v1/churn/{user_id}")
+@app.get("/api/churn/{user_id}")
 def get_prediction_for_item(user_id: str):
     return {"user_id": user_id, "score": random.random()}
+
+# обрабатываем GET-запросы по пути /v1/credit/{client_id} для получения 
+# ответ выдать или нет кредит на основании кредитного score клиента
+@app.get("/api/credit/{client_id}")
+def is_credit_approved(client_id: str):
+    score = random.random()
+    if score > 0.8:
+        approved = 1
+    else:
+        approved = 0
+    return {"client_id": client_id, "approved": approved}
